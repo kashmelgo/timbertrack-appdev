4b91ac99b1924626d2b162be4ab6e60e5a9a8356
kashmelgo <55750939+kashmelgo@users.noreply.github.com>
2021-12-11 15:56:25 +0800

    Revert "Merge from tasks"
    This reverts commit 5a23a0717b320252692d69d51813b9917cc5d7c0.

--- timbertrack/resources/js/Pages/Task.js
+++ timbertrack/resources/js/Pages/Task.js
@@ -3,6 +3,16 @@ import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
 import Form from './TaskComponents/TaskForm';
 import TaskDisplay from './TaskComponents/TaskDisplay';
+<<<<<<< HEAD
+
+
+export default function Task(props) {
+
+    const [inputText, setInputText] = useState("");
+    const [employee, setEmployee] = useState([]);
+    const [day, setDay] = useState("Today");
+    const [employed, setEmployed] = useState([]);
+=======
 import { Inertia } from '@inertiajs/inertia';
 
 
@@ -21,6 +31,7 @@ export default function Task(props) {
     console.log(employee);
 
     const [inputText, setInputText] = useState("");
+>>>>>>> tasks
 
 
     return (
@@ -34,6 +45,36 @@ export default function Task(props) {
 
 
             <div className="py-12">
+<<<<<<< HEAD
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                     {/* admin */}
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                    <div className="p-5 bg-white border-b border-gray-200 ">
+                        <Form
+                             inputText={inputText}
+                             setInputText={setInputText}
+                             day = {day}
+                             setDay={setDay}
+                             employee = {employee}
+                             employed = {employed}
+                             setEmployed={setEmployed}
+                             setEmployee = {setEmployee}
+
+                        />
+
+
+                    </div>
+
+
+                    </div>
+                     {/* end admin */}
+                    <TaskDisplay
+                         employed = {employed}
+                         employee = {employee}
+                         setEmployee = {setEmployee}
+                         />
+=======
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
 
@@ -58,6 +99,7 @@ export default function Task(props) {
                         List = {props.task}
                         user = {props.auth.user}
                         />
+>>>>>>> tasks
                 </div>
             </div>
         </Authenticated>
--- timbertrack/resources/js/Pages/TaskComponents/TaskDisplay.js
+++ timbertrack/resources/js/Pages/TaskComponents/TaskDisplay.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ValuesDisplay from '@/Pages/TaskComponents/ValuesDisplay'
 
+<<<<<<< HEAD
+export default function TaskDisplay({employee, setEmployee,  employed}) {
+=======
 export default function TaskDisplay({employee, List, usertype, user}) {
     const current = new Date();
     const date= `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`;
+>>>>>>> tasks
 
     return (
         <div className="my-5  flex space-x-10 ">
@@ -12,6 +16,17 @@ export default function TaskDisplay({employee, List, usertype, user}) {
 
                         <h1>Past Due</h1>
 
+<<<<<<< HEAD
+                        {employee.map( employees => {
+                            if(employees.employee === employed && employees.day === 'Past'  ){
+                                return(
+                                    <ValuesDisplay
+                                    key= {employees.id}
+                                    setEmployee = {setEmployee}
+                                    employees={employees}
+                                    employee={employee}
+                                    task={employees.task}
+=======
                         {List.map( list => {
 
                             if(list.day == date  && list.employee_id == employee.employee_id && (list.employee_id == user.id || user.usertype =="admin")){
@@ -22,10 +37,15 @@ export default function TaskDisplay({employee, List, usertype, user}) {
                                         employee={employee}
                                         task={list.task_name}
                                         usertype = {user.usertype}
+>>>>>>> tasks
                                     />
                                 );
                             }
                             })}
+<<<<<<< HEAD
+
+=======
+>>>>>>> tasks
                     </center>
                 </div >
 
@@ -33,6 +53,26 @@ export default function TaskDisplay({employee, List, usertype, user}) {
                     <center>
                         <h1>Today</h1>
 
+<<<<<<< HEAD
+                        {employee.map( employees => {
+                            if(employees.employee === employed && employees.day === 'Today'){
+                                return(
+                                    <ValuesDisplay
+                                    key= {employees.id}
+                                    setEmployee = {setEmployee}
+                                    employees={employees}
+                                    employee={employee}
+                                    task={employees.task}
+                                    />
+                                );
+
+                            }
+                            })}
+
+                    </center>
+                </div>
+
+=======
                          {List.map( list => {
 
                             if(list.day == date  && list.employee_id == employee.employee_id && (list.employee_id == user.id || user.usertype =="admin")){
@@ -50,10 +90,22 @@ export default function TaskDisplay({employee, List, usertype, user}) {
                             })}
                     </center>
                 </div>
+>>>>>>> tasks
                 <div className= "bg-white container shadow-lg px-6 py-2 h-auto w-1/3">
                     <center>
                         <h1>Tomorrow</h1>
 
+<<<<<<< HEAD
+                        {employee.map( employees => {
+                            if(employees.employee === employed && employees.day === 'Tomorrow'){
+                                return(
+                                    <ValuesDisplay
+                                    key= {employees.id}
+                                    setEmployee = {setEmployee}
+                                    employees={employees}
+                                    employee={employee}
+                                    task={employees.task}
+=======
                         {List.map( list => {
 
                             if(list.day == date  && list.employee_id == employee.employee_id && (list.employee_id == user.id || user.usertype =="admin")){
@@ -65,13 +117,26 @@ export default function TaskDisplay({employee, List, usertype, user}) {
                                         employee={employee}
                                         task={list.task_name}
                                         usertype = {user.usertype}
+>>>>>>> tasks
                                     />
                                 );
                             }
                             })}
 
+<<<<<<< HEAD
+
+
+
+                    </center>
+                </div>
+
+        </div>
+
+
+=======
                     </center>
                 </div>
         </div>
+>>>>>>> tasks
     );
 }
